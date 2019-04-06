62b87d2d3e7973fbf4863da61dc5451eb7d6e5f0
Ana <alcvlara@sga.pucminas.br>
2019-04-06 17:47:40 -0300

    Initial commit

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteTextLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var authorImageView: UIImageView!
+    
+    var quoteBook = QuoteBook()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func getNewQuote () {
+        let newQuote = quoteBook.getRandomQuote()
+        let author = quoteBook.getAuthor(for: newQuote)
+        let authorImageName = quoteBook.getImageName(for: author)
+        
+        self.quoteTextLabel.text = newQuote
+        self.authorNameLabel.text = author
+        self.authorImageView.image = UIImage(named: authorImageName)
+    }
 }
 
